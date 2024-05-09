68618b44b3562dd3eff9d9839688f7fb4991639c
hasanmahi <mhasan@purpletransform.com>
2024-05-09 14:49:20 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const mqtt = require("mqtt");
+const mongoose = require("mongoose");
+
+// MongoDB connection
+mongoose.connect("mongodb://localhost:27017/mqtt_datastore", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const TemperatureData = mongoose.model("TemperatureData", {
+  temperature: Number,
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// MQTT connection options
+const mqttOptions = {
+  host: "mqtt://192.168.0.180",
+  port: 1883, // Default MQTT port
+};
+
+// Connect to MQTT broker
+const client = mqtt.connect(mqttOptions);
+
+// MQTT connection event handlers
+client.on("connect", function () {
+  console.log("Connected to MQTT broker");
+  // Subscribe to the temperature topic
+  client.subscribe("temperature");
+});
+
+client.on("error", function (error) {
+  console.error("Error:", error);
+});
+
+// MQTT message handler
+client.on("message", function (topic, message) {
+  console.log("Received message:", message.toString(), "on topic:", topic);
+
+  if (topic === "temperature") {
+    const temperature = parseFloat(message.toString());
+    if (!isNaN(temperature)) {
+      // Save temperature data to MongoDB
+      const data = new TemperatureData({
+        temperature: temperature,
+      });
+
+      data
+        .save()
+        .then(() => console.log("Temperature data saved to MongoDB"))
+        .catch((err) => console.error("Error saving temperature data:", err));
+    } else {
+      console.error("Invalid temperature value received:", message.toString());
+    }
+  }
+});
